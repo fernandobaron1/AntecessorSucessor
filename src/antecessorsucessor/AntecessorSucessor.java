b35684c99a2217345eb90014e1050ca9f4b71e63
Fernando <Fernando@DESKTOP-5TCTFP0>
2019-06-21 23:38:20 -0300

    Terceiro Commit

--- src/antecessorsucessor/AntecessorSucessor.java
+++ src/antecessorsucessor/AntecessorSucessor.java
@@ -6,6 +6,16 @@ public class AntecessorSucessor {
 
     
     public static void main(String[] args) {
+        
+        int numero = 2;
+        int sucessor = 0;
+        int antecessor = 0;
+        
+        antecessor = numero - 1;
+        sucessor = numero + 1;
+        
+        System.out.println("Número Antecessor: "+antecessor);
+        System.out.println("Número Sucessor: "+sucessor);
        
     }
     
